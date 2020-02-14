362276ea8212c70b793bb14758ae0a3ddd4f580f
Project-Creator <47641284+Project-Creator@users.noreply.github.com>
2020-02-14 17:58:05 -0500

    Create JfacAlpha.js
    My first Javascript library, designed to ease the use of javascript to modify CSS and add files. This, the Alpha version also contains a property for random numbers.

---
+++ Jfac/JfacAlpha.js
@@ -0,0 +1,52 @@
+window.Jfac = {
+    css: {
+        add: function(id, action) {
+            this.id = id;
+            this.action = action;
+            if (id.substr(0, 1) === "#") {
+                document.getElementsByTagName("style")[0].innerHTML = document.getElementsByTagName("style")[0].innerHTML + id + "{" + action + "}";
+            }
+            if (id.substr(0, 1) === ".") {
+                document.getElementsByTagName("style")[0].innerHTML = document.getElementsByTagName("style")[0].innerHTML + id + "{" + action + "}";
+            }
+            //console.log(id+" , "+action);
+        },
+        get: function(id, style_type) {
+            if (id.substr(0, 1) === "#") {
+                return document.getElementsByTagName("style")[0].innerHTMLwindow.getComputedStyle(element);
+            }
+            if (id.substr(0, 1) === ".") {
+                return document.getElementsByTagName("style")[0].innerHTMLwindow.getComputedStyle(element);
+            }
+        }
+    },
+    file: {
+        bring: function(src, type) {
+            this.src = src;
+            this.type = type;
+            var node = document.createElement("script");
+            document.getElementsByTagName("body")[0].appendChild(node);
+            node.setAttribute("src", src);
+            node.setAttribute("type", type);
+            console.log("added script " + src + ", type = " + type);
+        },
+        remove: function(src, type) {
+            this.src = src;
+            this.type = type;
+            document.querySelector("script", src).remove();
+        }
+    },
+    math: {
+        rand: function(min, max,whole) {
+            this.max = max;
+            this.min = min;
+            this.whole = inc;
+            if (inc === true) {
+                return Math.floor(Math.random() * max + min);
+            }
+            if (inc === false){
+                return math.random() * max + min;
+            }
+        },
+    },
+};
