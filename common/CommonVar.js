12cb49ec5adc9a1186c04e1c0177e508e721d3ed
Wookiesrule22(project-Creator) <47641284+Project-Creator@users.noreply.github.com>
2020-05-08 21:00:16 -0400

    Update CommonVar.js

--- common/CommonVar.js
+++ common/CommonVar.js
@@ -1,22 +1,32 @@
-let red = "red";
-let orange = "orange";
-let yellow = "yellow";
-let green = "green";
-let blue = "blue";
-let indigo = "indigo";
-let violet = "violet";
-let color = "color";
-let black = "black";
-let background = "background";
-let border = "border";
-let body = "body";
-let p = "p";
-let div = "div";
-let span = "span";
-let h1 = "h1";
-let h2 = "h2";
-let h3 = "h3";
-let h4 = "h4";
-let h5 = "h5";
-let h6 = "h6";
-let script = "script";
+var temp;
+function preventDefCommon(){
+  temp = false;
+} 
+if(temp != false){
+  let red = "red";
+  let orange = "orange";
+  let yellow = "yellow";
+  let green = "green";
+  let blue = "blue";
+  let indigo = "indigo";
+  let violet = "violet";
+  let color = "color";
+  let black = "black";
+  let background = "background";
+  let border = "border";
+  let body = "body";
+  let p = "p";
+  let div = "div";
+  let span = "span";
+  let h1 = "h1";
+  let h2 = "h2";
+  let h3 = "h3";
+  let h4 = "h4";
+  let h5 = "h5";
+  let h6 = "h6";
+  let script = "script";
+  let em = "em";
+  let br = "br";
+  let a = "a";
+  let link = "link";
+}
