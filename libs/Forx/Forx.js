9cdc0fa9a5350aae509677ef5cf6984894ef1d44
Wookiesrule22(project-Creator) <47641284+Project-Creator@users.noreply.github.com>
2020-05-26 14:49:29 -0400

    Create Forx.js

---
+++ libs/Forx/Forx.js
@@ -0,0 +1,30 @@
+var Forx = {
+  main: function() {
+    function New(a, b, c, d) {
+      let temp = document.querySelectorAll("[" + a + "]");
+      for (var i = 0; i < temp.length; i++) {
+        let tempa;
+        let tempb = false;
+        for (var z; z < c.length; z++) {
+          if (temp[i].getAttribute(a).search(c[z]) != -1) {
+            tempb = true;
+            break;
+          }
+        }
+        if (tempb == false) {
+          tempa = d;
+        }
+        temp[i].style[b] = temp[i].getAttribute(a) + tempa;
+      }
+    }
+    New('c', "background-color", [], "");
+    New('w', "width", ["px", "%", "vw", "vh", "em"], "%");
+    New('h', "height", ["px", "%", "vw", "vh", "em"], "%");
+    New('bw', "border-width", ["px", "%", "vw", "vh", "em"], "px");
+    New('bc', "border-color", [], "");
+    New('bs', "border-style", [], "");
+    New('br', "border-radius", ["px", "%", "vw", "vh", "em"], "px");
+    New('b', "border", [], "");
+  },
+};
+Forx.main();
